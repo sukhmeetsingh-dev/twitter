3fe4635e3a16a710313578979afa161ac1e07d36
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-08-14 23:46:47 +0530

    add javascript

---
+++ script.js
@@ -0,0 +1,34 @@
+//  DOM elements
+
+const mainPage = document.querySelector('.main-page');
+const loginPage = document.querySelector('.login-page');
+const middleContent = document.querySelector('.middle-content');
+const btnTop = document.querySelector('.btn-top');
+const newsFeedPage = document.querySelector('.feeds-page');
+
+// Main Page
+
+const goToLoginPage = () => {
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
+};
+
+middleContent.addEventListener('click',e => {
+    if(e.target.classList[1] === 'main-btn'){
+        goToLoginPage();
+    }
+});
+
+btnTop.addEventListener('click',() => {
+    const inputuserInfo = document.querySelector('.user-info');
+    const inputPassword = document.querySelector('.password');
+
+    if(inputuserInfo.value !== '' && inputPassword.value !== ''){
+        mainPage.style.display = 'none';
+        newsFeedPage.style.display = 'block';
+    }
+    else{
+        goToLoginPage();
+    }
+
+});
