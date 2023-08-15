0335e925178871e4b59c9dcccfb2fe1309dde2f6
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-08-15 23:43:28 +0530

    added javascript

--- script.js
+++ script.js
@@ -5,14 +5,16 @@ const loginPage = document.querySelector('.login-page');
 const middleContent = document.querySelector('.middle-content');
 const btnTop = document.querySelector('.btn-top');
 const newsFeedPage = document.querySelector('.feeds-page');
+const loginModal = document.querySelector('.login-modal');
+const modalX = document.querySelector('.login-modal i');
+const loginFormBtn = document.querySelector('.login-form-btn');
 
 // Main Page
 
 const goToLoginPage = () => {
-    mainPage.style.display = 'none';
-    loginPage.style.display = 'grid';
+mainPage.style.display = 'none';
+loginPage.style.display = 'grid';
 };
-
 middleContent.addEventListener('click',e => {
     if(e.target.classList[1] === 'main-btn'){
         goToLoginPage();
@@ -29,6 +31,27 @@ btnTop.addEventListener('click',() => {
     }
     else{
         goToLoginPage();
+        loginModal.style.display = 'block';
     }
 
+});
+
+                // Login page
+
+modalX.addEventListener('click', () => {
+    loginModal.style.display = 'none';
+});
+
+loginFormBtn.addEventListener('click', () =>{
+    const loginUserInfo = document.querySelector('.login-user-info');
+
+    const loginPassword = document.querySelector('.login-password');
+
+    if(loginUserInfo.value !== "" && loginPassword.value !== ""){
+        loginPage.style.display = 'none';
+        newsFeedPage.style.display = 'block';
+    }
+    else{
+        loginModal.style.display = 'block';
+    }
 });
