e07485385dd2521fabe623763a5e9bb60f8b9268
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-08-16 23:33:13 +0530

    added javascript and post modal box

--- script.js
+++ script.js
@@ -8,50 +8,90 @@ const newsFeedPage = document.querySelector('.feeds-page');
 const loginModal = document.querySelector('.login-modal');
 const modalX = document.querySelector('.login-modal i');
 const loginFormBtn = document.querySelector('.login-form-btn');
+const postBtn = document.querySelector('.post-btn');
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postModalX = document.querySelector('.modal-header i');
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
+const modalInput = document.querySelector('.modal-input');
 
 // Main Page
 
 const goToLoginPage = () => {
-mainPage.style.display = 'none';
-loginPage.style.display = 'grid';
+    mainPage.style.display = 'none';
+    loginPage.style.display = 'grid';
 };
-middleContent.addEventListener('click',e => {
-    if(e.target.classList[1] === 'main-btn'){
+middleContent.addEventListener('click', e => {
+    if (e.target.classList[1] === 'main-btn') {
         goToLoginPage();
     }
 });
 
-btnTop.addEventListener('click',() => {
+btnTop.addEventListener('click', () => {
     const inputuserInfo = document.querySelector('.user-info');
     const inputPassword = document.querySelector('.password');
 
-    if(inputuserInfo.value !== '' && inputPassword.value !== ''){
+    if (inputuserInfo.value !== '' && inputPassword.value !== '') {
         mainPage.style.display = 'none';
         newsFeedPage.style.display = 'block';
     }
-    else{
+    else {
         goToLoginPage();
         loginModal.style.display = 'block';
     }
 
 });
 
-                // Login page
+// Login page
 
 modalX.addEventListener('click', () => {
     loginModal.style.display = 'none';
 });
 
-loginFormBtn.addEventListener('click', () =>{
+loginFormBtn.addEventListener('click', () => {
     const loginUserInfo = document.querySelector('.login-user-info');
 
     const loginPassword = document.querySelector('.login-password');
 
-    if(loginUserInfo.value !== "" && loginPassword.value !== ""){
+    if (loginUserInfo.value !== "" && loginPassword.value !== "") {
         loginPage.style.display = 'none';
         newsFeedPage.style.display = 'block';
     }
-    else{
+    else {
         loginModal.style.display = 'block';
+    }
+});
+// News feed page 
+// Post modal
+postBtn.addEventListener('click', () => {
+    modal.style.display = 'block';
+    modalWrapper.classList.add('modal-wrapper-display');
+});
+
+const changeOpacity = x => {
+    modalPostBtn.style.opacity = x;
+    modalFooterPlus.style.opacity = x;
+};
+
+postModalX.addEventListener('click', () => {
+    modal.style.display = 'none';
+    modalWrapper.classList.remove('modal-wrapper-display');
+
+    if(modalInput.value !== ""){
+        modalInput.value = "" ;
+        changeOpacity(0.5);
+    }
+});
+
+modalInput.addEventListener('keypress',(e) =>{
+    if(e.target.value !== ""){
+        changeOpacity(1);
+    }
+});
+
+modalInput.addEventListener('blur',(e) =>{
+    if(e.target.value === ""){
+        changeOpacity(0.5);
     }
 });
