02c703efa15e2b757f993c2a7e0bd4e60d1d6841
Sukhmeet <sukhmeetsingh645@gmail.com>
2023-08-18 18:45:29 +0530

    added darkmode

--- script.js
+++ script.js
@@ -19,6 +19,9 @@ const user = document.querySelector('.user');
 const sidebar = document.querySelector('.sidebar');
 const sidebarWrapper = document.querySelector('.sidebar-wrapper');
 const xBtn = document.querySelector('.sidebar-header i');
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
+
 
 // Main Page
 
@@ -82,30 +85,45 @@ postModalX.addEventListener('click', () => {
     modal.style.display = 'none';
     modalWrapper.classList.remove('modal-wrapper-display');
 
-    if(modalInput.value !== ""){
-        modalInput.value = "" ;
+    if (modalInput.value !== "") {
+        modalInput.value = "";
         changeOpacity(0.5);
     }
 });
 
-modalInput.addEventListener('keypress',(e) =>{
-    if(e.target.value !== ""){
+modalInput.addEventListener('keypress', (e) => {
+    if (e.target.value !== "") {
         changeOpacity(1);
     }
 });
 
-modalInput.addEventListener('blur',(e) =>{
-    if(e.target.value === ""){
+modalInput.addEventListener('blur', (e) => {
+    if (e.target.value === "") {
         changeOpacity(0.5);
     }
 });
 // sidebar
-user.addEventListener('click', ()=> {
+user.addEventListener('click', () => {
     sidebar.classList.add('sidebar-display');
     sidebarWrapper.classList.add('sidebar-wrapper-display');
 });
 
-xBtn.addEventListener('click',() =>{
+xBtn.addEventListener('click', () => {
     sidebar.classList.remove('sidebar-display');
     sidebarWrapper.classList.remove('sidebar-wrapper-display');
+});
+
+// dark mode
+
+const darkElements1 = document.querySelectorAll('.dark-mode-1');
+const darkElements2 = document.querySelectorAll('.dark-mode-2');
+
+
+toggle.addEventListener('click', () => {
+    circle.classList.toggle('move');
+    Array.from(darkElements1).map(darkElements1 =>
+        darkElements1.classList.toggle('dark-1'));
+
+    Array.from(darkElements2).map(darkElements2 =>
+        darkElements2.classList.toggle('dark-2'));
 });
